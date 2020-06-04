7e0c34e463af1c9781aaab62fca6b52c254c5397
YuERA1992 <yuzaburo.era@jointcrew.co.jp>
2020-06-05 08:39:19 +0900

    信号機の色を変えるアプリ作った

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <template>
   <div id="app">
+    <!-- <p v-show="toggle">
+      Hello ERA
+    </p>
+    <p>
+      <helloConponent></helloConponent>
+    </p> -->
+    
+    <!-- <p v-else>
+
+    </p> -->
+    <p>信号機の色を変える</p>
+    <button v-on:click="onClick">変えちゃうぞ</button>
+    <br>
+    <br>
+    <!-- <p>{{colors}}</p> -->
+    <p>信号機の色は……</p>
+    <p v-if="color==='red'" style="color:red;background-color:black">
+      赤やで！Stop
+    </p>
+    <p v-else-if="color==='yellow'" style="color:yellow; background-color:black">
+      黄色です！Caution
+    </p>
+    <p v-else-if="color==='green'" style="color:green; background-color:black">
+      青じゃん！Go
+    </p>
+    <p v-else>
+      故障中
+    </p>
   </div>
 </template>
 
@@ -7,6 +35,24 @@
 
 export default {
   name: 'App',
+  data(){
+    return{
+      // toggle:false 
+      colors: ['red','yellow','green'],
+      color: null,
+      i:0
+    }
+  },
+  methods:{
+    onClick(){
+      this.color = this.colors[this.i]
+      if (this.i<3){this.i ++}
+      else{this.i=0}
+    }
+  }
+  // components:{
+  //   helloConponent:{template:'<p>Hello</p>'}
+  // }
 }
 </script>
 
